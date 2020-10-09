56dc23852a9d0c3463ac9f55f6023fa99d329137
Amiran <amiranlevanovich@gmail.com>
2020-10-09 12:20:08 +0300

    view show fixed

--- spec/views/user/show.html.erb_spec.rb
+++ spec/views/user/show.html.erb_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
   let(:user) { create(:user, name: 'Test') } # нам нужен залогиненный юзер
-  let(:game) { create(:game, id: 15, created_at: Time.parse('2016.10.09, 13:00'), current_level: 10, prize: 1000
-  ) }
   
   before(:each) do
     assign(:user, user) # назначаем юзера
     sign_in user # log in
-    assign(:game, [ build_stubbed(:game)]) # назначаем игру
-    allow(game).to receive(:status).and_return(:in_progress)
-    render partial: 'users/game', object: game
+    assign(:games, [ build_stubbed(:game)]) # назначаем игру
+
     render
   end
   
